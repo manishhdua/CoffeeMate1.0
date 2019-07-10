2f4d488f9690926c6e4ce22facfdc8949ed1e46a
manishdua <mdua007@gmail.com>
2019-07-10 07:39:35 -0400

    CoffeMate1.0 inClass Assignment

---
+++ app/src/main/java/ie/cm/activities/Contact.java
@@ -0,0 +1,17 @@
+package ie.cm.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import ie.cm.R;
+
+public class Contact extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact);
+
+
+    }
+}
